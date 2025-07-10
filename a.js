7f60fdac1d63a13c92ff13757f04e2e019baebd5
artem <dan119@bk.ru>
2025-07-10 20:30:49 +0300

    aa

--- a.js
+++
@@ -1,6 +0,0 @@
-import query from './src/models/connect_to_database.js';
-const table = 'filter_up'
-query(`select * from ${table}`)
-(async()=>{
-    console.log(await query`select * from ${query.unsafe(table)}`)
-})
